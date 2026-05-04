ab2c549cbbd843bf17e1724319e42019d271c8b2
Mashuk Tamim <mashuk.tamim@exabyting.com>
2026-05-04 09:00:25 +0600

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu(
+  {
+    type: "app",
+    react: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+  },
+  {
+    rules: {
+      "ts/no-redeclare": "off",
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+    },
+  },
+);
